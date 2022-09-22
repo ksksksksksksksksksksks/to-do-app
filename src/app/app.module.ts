717df52b6c5f9e166d7f602c7db3fdb9bbdaaa6e
Сумаренко Екатерина Михайловна <sumarenka@sc.softclub.by>
2022-09-22 16:56:28 +0300

    Add navigation with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'todo', component: TodoComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
