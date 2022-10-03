7db3214fb8b5aa96b61bb7f9cf8589c55797595e
Сумаренко Екатерина Михайловна <sumarenka@sc.softclub.by>
2022-10-03 18:08:55 +0300

    Code improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationService } from './authentication.service';
 import { LoginComponent } from './login/login.component';
